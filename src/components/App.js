2c42d917f79065f604a4af304cde2b976f30bcfe
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-07-29 18:21:25 -0400

    Updated layout. Added copy button

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import FoxCard from './FoxCard'
 import { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import { useSpring, animated } from 'react-spring'
+import logo from '../images/flex-fox.png'
 
 function App() {
   const [flexDirValue, setFlexDirValue] = useState("row")
   const [flexWrapValue, setFlexWrapValue] = useState("wrap")
   const [justifyContentValue, setJustifyContentValue] = useState("space-between")
   const [alignItemsValue, setAlignItemsValue] = useState("center")
+  const [wasClicked, setWasClicked] = useState(false)
   const [foxes, setFoxes] = useState([])
 
   const FlexContainer = styled.div`
@@ -21,7 +24,6 @@ function App() {
     margin-right: 10%;
   `
 
-  // Number randomizer
   function getRandomInt(max) {
     let randNum = Math.floor(Math.random() * max);
     if (randNum > 0) {
@@ -31,12 +33,11 @@ function App() {
     }
   }
 
-  // Generates a random array of foxes on page load
   useEffect(() => {
     setFoxes(function() {
         let arr = []
         let counter = 0
-        while (counter < getRandomInt(201) ) {
+        while (counter < getRandomInt(201)) {
           counter += 1
           arr = [...arr, {img: `https://randomfox.ca/images/${getRandomInt(124)}.jpg`, num: counter} ]
         }
@@ -44,7 +45,6 @@ function App() {
       })
   }, [])
   
-  // Returns an array of FoxCard components for each fox to render
   const foxPics = foxes.map(function(foxPic) {
     return <FoxCard key={foxPic.num}
                     img={foxPic.img}
@@ -52,52 +52,92 @@ function App() {
           />
   })
 
+  const copy = (val) => {
+    let dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.setAttribute("id", "dummy_id");
+    document.getElementById("dummy_id").value=val;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+    setWasClicked(true)
+  }
+
+  const anim = useSpring({
+    config: {
+      duration: 500
+    },
+    opacity: wasClicked ? 1 : 0
+  })
+
   return (
     <>
       <div className="page">
         <div className="sticky">
           <div id="header">
-            <img src="https://i.imgur.com/m3t26eT.png" alt="logo" style={{height: "80px"}}/>
-            <h2>An adorable Flexbox visualizer.</h2>
-            <h3>Made with the <a href="https://randomfox.ca/">Random Fox API.</a></h3>
-          </div>
-          <div id="filters">
-              <span className="select-label select-label-bold">.container &#123; </span><span className="select-label">display: flex; flex-direction:</span>
-              <select name="flex-direction" onChange={e => setFlexDirValue(e.target.value)}>
-                <option value="row">row;</option>
-                <option value="row-reverse">row-reverse;</option>
-                <option value="column">column;</option>
-                <option value="column-reverse">column-reverse;</option>
-              </select>
-              <span className="select-label"> flex-wrap:</span>
-              <select name="flex-wrap" onChange={e => setFlexWrapValue(e.target.value)}>
-                <option value="wrap">wrap;</option>
-                <option value="wrap-reverse">wrap-reverse;</option>
-                <option value="nowrap">nowrap;</option>
-              </select>
-              <span className="select-label"> justify-content:</span>
-              <select name="justify-content" onChange={e => setJustifyContentValue(e.target.value)}>
-                <option value="space-between">space-between;</option>
-                <option value="space-evenly">space-evenly;</option>
-                <option value="space-around">space-around;</option>
-                <option value="center">center;</option>
-                <option value="flex-start">flex-start;</option>
-                <option value="flex-end">flex-end;</option>
-              </select>
-              <span className="select-label"> align-items:</span>
-              <select name="justify-content" onChange={e => setAlignItemsValue(e.target.value)}>
-                <option value="center">center;</option>
-                <option value="flex-start">flex-start;</option>
-                <option value="flex-end">flex-end;</option>
-              </select>
-              <span className="select-label select-label-bold"> &#125;</span>
+            <div className="top">
+              <img src={logo} alt="logo" style={{height: "10rem"}}/>
+              <h2>An adorable Flexbox visualizer.</h2>
+              <h3>Made with the <a href="https://randomfox.ca/" target="_blank" rel="noreferrer">Random Fox API.</a></h3>
+            </div>
+            <div id="filters">
+              <div className="form-container">
+                <div className="select-label select-label-bold" id="top-label">.container &#123; </div><br/>
+                <div className="select-block">
+                  <div className="select-label">display: <span id="flex" style={{ color: "#4b4b4b", fontWeight: "normal" }}>flex;</span></div><br/>
+                </div>
+                <div className="select-block">
+                  <span className="select-label">flex-direction:</span>
+                  <select name="flex-direction" className="dropdown" onChange={e => {setFlexDirValue(e.target.value); setWasClicked(false)}}>
+                    <option value="row">row;</option>
+                    <option value="row-reverse">row-reverse;</option>
+                    <option value="column">column;</option>
+                    <option value="column-reverse">column-reverse;</option>
+                  </select><br/>
+                </div>
+                <div className="select-block">
+                  <span className="select-label"> flex-wrap:</span>
+                  <select name="flex-wrap" onChange={e => {setFlexWrapValue(e.target.value); setWasClicked(false)}}>
+                    <option value="wrap">wrap;</option>
+                    <option value="wrap-reverse">wrap-reverse;</option>
+                    <option value="nowrap">nowrap;</option>
+                  </select><br/>
+                </div>
+                <div className="select-block">
+                  <span className="select-label"> justify-content:</span>
+                  <select name="justify-content" onChange={e => {setJustifyContentValue(e.target.value); setWasClicked(false)}}>
+                    <option value="space-between">space-between;</option>
+                    <option value="space-evenly">space-evenly;</option>
+                    <option value="space-around">space-around;</option>
+                    <option value="center">center;</option>
+                    <option value="flex-start">flex-start;</option>
+                    <option value="flex-end">flex-end;</option>
+                  </select><br/>
+                </div>
+                <div className="select-block">
+                  <span className="select-label"> align-items:</span>
+                  <select name="justify-content" onChange={e => {setAlignItemsValue(e.target.value); setWasClicked(false)}}>
+                    <option value="center">center;</option>
+                    <option value="flex-start">flex-start;</option>
+                    <option value="flex-end">flex-end;</option>
+                  </select><br/>
+                </div>
+                <span className="select-label select-label-bold" id="bottom-label"> &#125;</span>
+              </div>
+              <button onClick={() => copy(`display: flex;\nflex-direction: ${flexDirValue};\nflex-wrap: ${flexWrapValue};\njustify-content: ${justifyContentValue};\nalign-items: ${alignItemsValue};`)}>{ !wasClicked ? "Flex on me!" : <animated.span style={anim}>Copied code!</animated.span> }</button>
+            </div>
+            <div className="footer">
+              <p>Designed by <a href="https://zachkaigler.com/" target="_blank" rel="noreferrer">Zach Kaigler</a></p>
+            </div>
           </div>
         </div>
-        <FlexContainer>
-          {foxPics}
-        </FlexContainer>
+        <div className="main-content">
+          <FlexContainer>
+            {foxPics}
+          </FlexContainer>
+        </div>
       </div>
-      <a href="https://github.com/zachkaigler/flex-fox" id="github"><img src="https://i.imgur.com/Yt24doC.png" alt="github" style={{ height: "75px" }} /></a>
+      <a href="https://github.com/zachkaigler/flex-fox" id="github" target="_blank" rel="noreferrer"><img src="https://i.imgur.com/Yt24doC.png" alt="github" style={{ height: "75px" }} /></a>
     </>
   );
 }
